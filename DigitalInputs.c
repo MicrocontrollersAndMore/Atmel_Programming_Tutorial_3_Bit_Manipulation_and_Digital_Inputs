63e982187c8f0f9377acc1910b757e827ba2e055
MicrocontrollersAndMore <cdahms123@gmail.com>
2015-11-11 08:01:37 -0500

    1

--- DigitalInputs.c
+++ DigitalInputs.c
@@ -1,23 +1,34 @@
-// MyAtmelBlink.c
+// DigitalInputs.c
 
-// put LED on PC5
+// switch on PD2 (pin 4)
+// LED on PC5 (pin 28), turns on/off based on switch press
 
 #ifndef F_CPU					// if F_CPU was not defined in Project -> Properties
 #define F_CPU 1000000UL			// define it now as 1 GHz unsigned long
 #endif
 
 #include <avr/io.h>				// this is always included in AVR programs
-#include <util/delay.h>			// add this to use the delay function
+
+#define BIT_IS_SET(byte, bit) (byte & (1 << bit))
+#define BIT_IS_CLEAR(byte, bit) (!(byte & (1 << bit)))
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(void) {
 	
+	DDRD &= ~(1 << PD2);		// clear DDRD bit 2, sets PD2 (pin 4) for input
+	
+	PORTD |= (1 << PD2);		// set PD2/INT0 (pin 4) internal pull-up resistor
+	
 	DDRC |= (1 << PC5);			// set Port C pin PC5 for output
 	
 	while (1) {					// begin infinite loop
-		PORTC ^= (1 << PC5);	// flip state of LED on PC5
-		_delay_ms(500);			// delay 1/2 second
+		if(BIT_IS_CLEAR(PIND, PD2)) {
+			PORTC |= (1 << PC5);
+		} else if(BIT_IS_SET(PIND, PD2)) {
+			PORTC &= ~(1 << PC5);
+		} else {
+			// ideally should never get here, but may occasionally due to timing
+		}
 	}
 	return(0);					// should never get here, this is to prevent a compiler warning
 }
-
